2dd008673e78a4d6b62375138b3aa68a6c6e3975
hyperkun <admin@hyperchan.org.ua>
2018-07-18 00:02:44 +0300

    Allow all word characters to be present in board ID

--- posts/urls.py
+++ posts/urls.py
@@ -6,18 +6,18 @@ urlpatterns = [
     url(
         r'^$', render_index, name="index"),
     url(
-        r'^(?P<board_name>[A-Za-z]+)/res/(?P<thread_id>[0-9]+).html$',
+        r'^(?P<board_name>\w+)/res/(?P<thread_id>[0-9]+).html$',
         render_thread,
         name="thread"
         ),
-    url(r'^(?P<board_name>[A-Za-z]+)/$', render_board, name="board"),
+    url(r'^(?P<board_name>\w+)/$', render_board, name="board"),
     url(
-        r'^(?P<board_name>[A-Za-z]+)/(?P<current_page>[0-9]+).html$',
+        r'^(?P<board_name>\w+)/(?P<current_page>[0-9]+).html$',
         render_board,
         name="board_page"
         ),
     url(
-        r'^(?P<board_name>[A-Za-z]+)/catalog.html$',
+        r'^(?P<board_name>\w+)/catalog.html$',
         render_catalog,
         name="catalog"
         ),
